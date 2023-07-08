3396c0462266cc26ebbf8a2e2f6ef9fb6ace284d
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2023-07-08 17:03:12 +0530

    about me changed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,18 +5,21 @@ const About = () => {
       id="about"
       className="flex flex-col justify-center items-center m-4 gap-4 text-white "
     >
-      <div className="flex justify-center items-center gap-8 m-8">
-        <p className="text-right diff-font font-bold text-2xl xs:text-4xl md:text-6xl">
-          About
-          <br />
-          Me
+      <div className="flex flex-col xs:flex-row justify-center items-center gap-8 m-8">
+        <p className="text-center xs:text-right diff-font font-bold text-2xl xs:text-4xl md:text-6xl">
+          About <span className="inline xs:hidden">Me</span>
+          <br className="hidden xs:block"/>
+          <span className="hidden xs:block">Me</span>
         </p>
-        <p className="w-full xs:w-[60%] md:w-[40%] text-sm xs:text-base md:text-2xl">
-          <span className="font-bold">Hello! I'm Chaitanya Bhargava. </span>I am
-          a Web Developer proficient in Frontend with elementary knowledge of
-          Backend. I enjoy making colourful websites and sharing them. I have
-          worked on several solo as well as team projects in the past with
-          varied tech stacks.
+        <p className="w-full xs:w-[60%] lg:w-[50%] text-sm xs:text-base md:text-2xl">
+          <span className="font-bold">Hello! I'm Chaitanya Bhargava, </span>a
+          passionate Web Developer specializing in Frontend development with a
+          solid foundation in Backend technologies. With a keen eye for design
+          and a love for vibrant websites, I strive to create captivating online
+          experiences that leave a lasting impression. <br/> <br/> Moving forward, I'm excited to expand my horizons beyond web
+          development. I have a keen interest in exploring game development. Additionally, I'm eager to explore the integration of
+          machine learning technologies into my projects, pushing the boundaries
+          of what's possible in web applications.
         </p>
       </div>
       <div className="flex flex-col m-8">
@@ -32,12 +35,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="html5-color.svg"
                 alt="html5"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -45,12 +48,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="css3-color.svg"
                 alt="css3"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -58,12 +61,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="javascript-color.svg"
                 alt="javascript"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -71,12 +74,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="react-color.svg"
                 alt="react"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -84,12 +87,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="nextdotjs-color.svg"
                 alt="nextdotjs"
                 className="white-filter w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -97,12 +100,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="tailwindcss-color.svg"
                 alt="tailwindcss"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -117,12 +120,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="nodedotjs-color.svg"
                 alt="nodedotjs"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -130,12 +133,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="express-color.svg"
                 alt="express"
                 className="white-filter w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -143,12 +146,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="php-color.svg"
                 alt="php"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -156,12 +159,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="mysql-color.svg"
                 alt="mysql"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -169,12 +172,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="mongodb-color.svg"
                 alt="mongodb"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -191,12 +194,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="firebase-color.svg"
                 alt="firebase"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -204,12 +207,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="git-color.svg"
                 alt="git"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -217,12 +220,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="github-color.svg"
                 alt="github"
                 className="white-filter w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -230,12 +233,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="redux-color.svg"
                 alt="redux"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -243,12 +246,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="reactrouter-color.svg"
                 alt="reactrouter"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -263,12 +266,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="cplusplus-color.svg"
                 alt="cplusplus"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
@@ -276,12 +279,12 @@ const About = () => {
               <motion.img
                 drag
                 dragSnapToOrigin
-              dragConstraints={{
-                top: -25,
-                left: -25,
-                right: 25,
-                bottom: 25,
-              }}
+                dragConstraints={{
+                  top: -25,
+                  left: -25,
+                  right: 25,
+                  bottom: 25,
+                }}
                 src="python-color.svg"
                 alt="python"
                 className="w-[45px] h-[45px] xs:w-[60px] xs:h-[60px] m-4"
